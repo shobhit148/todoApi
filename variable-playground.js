753de3879e965785818640d3566f69e4d852b743
Shobhit <shobhitkumar@illuminz.com>
2017-03-02 23:42:16 +0530

    Added query string parameter

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+/*var person = {
+	name:'shobhit',
+	age:21
+};
+
+function updatePerson(obj){
+	obj = {
+		name:'shobhit',
+		age:24 
+	}
+
+	obj.age = 24
+}
+
+updatePerson(person);
+console.log(person);*/
+
+//array example
+
+var grades = [15, 20];
+
+function addGrade(gradesArr){
+	gradesArr.push(55);
+	debugger;
+
+	//gradesArr = [10,20,30]
+
+}
+
+addGrade(grades);
+console.log(grades);
