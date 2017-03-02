41cb092459e5daf61d0e668792854b52b7c24d9f
Shobhit <shobhitkumar@illuminz.com>
2017-03-02 22:02:32 +0530

    Refactor code with Without

--- server.js
+++ server.js
@@ -57,6 +57,21 @@ app.post('/todos', function (req, res){
 	res.json(body);
 });
 
+
+	//DELETE /todo/id
+	app.delete('/todos/:id', function (req, res){
+		var todoId = parseInt(req.params.id, 10);
+		var matchedTodo = _.findWhere(todos, {id:todoId});		
+
+		if(!matchedTodo){
+			res.status(404).json({"error": "no todo find with that id"});
+		}else{
+			todos = _.without(todos, matchedTodo);
+			res.json(matchedTodo);
+		}
+	})
+
+
 app.listen (PORT, function(){
 	console.log('Express listeing on port '+ PORT);
 }); 
