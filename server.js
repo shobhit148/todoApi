18141326f5912859ae52301fbd1d796fa49c2ded
Shobhit <shobhitkumar@illuminz.com>
2017-03-08 21:47:57 +0530

    convert get todos to sequelize

--- server.js
+++ server.js
@@ -19,9 +19,31 @@ app.get('/', function (req, res) {
 //http://localhost:3000/todos/3
 //GET /todos?completed=true&q=work
 app.get('/todos', function (req, res){
-	var queryparameter = req.query;
-	var filterTodos = todos;
+	//var queryparameter = req.query;
+	//var filterTodos = todos;
 
+	var query = req.query;
+	var where = {};
+
+	if(query.hasOwnProperty('completed') && query.completed == 'true'){
+		where.completed = true;
+	}else if(query.hasOwnProperty('completed') && query.completed == 'false'){
+		where.completed = false;
+	}
+
+	if(query.hasOwnProperty('q') && query.q.length>0){
+		where.description = {
+			$like : '%' + query.q + '%'
+		};
+	}
+
+	db.todo.findAll({where:where}).then(function (todos){
+		res.json(todos);
+	}, function (e){
+		res.status(500).send();
+	})
+
+/*
 	if(queryparameter.hasOwnProperty('completed') && queryparameter.completed === 'true'){
 		filterTodos = _.where(filterTodos, {completed:true})
 	}else if(queryparameter.hasOwnProperty('completed') && queryparameter.completed === 'false'){
@@ -34,7 +56,7 @@ app.get('/todos', function (req, res){
 		});	
 	}
 
-	res.json(filterTodos);
+	res.json(filterTodos);*/
 });
 
 //GET /todos/:id
