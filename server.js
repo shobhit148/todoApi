1cecd900452144a46051e80b18b2e54ffd44f7e3
Shobhit <shobhitkumar@illuminz.com>
2017-03-08 21:19:43 +0530

    Convert get todos id to use sequelize

--- server.js
+++ server.js
@@ -40,13 +40,16 @@ app.get('/todos', function (req, res){
 //GET /todos/:id
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id:todoId});
-	
-	if(matchedTodo){
-		res.json(matchedTodo)
-	}else{
-		res.status(404).send();
-	}
+
+	db.todo.findById(todoId).then(function(todo){
+		if(!!todo){
+			res.json(todo.toJSON());
+		}else{
+			res.status(404).send();	
+		}
+	}, function(e){
+		res.status(500).send();
+	});
 });
 
 //POST /todos:id
@@ -59,29 +62,6 @@ app.post('/todos', function (req, res){
 	}, function (e){
 		res.status(400).json(e);
 	});
-
-	//use _.pick to pick only description and completed
-
-	//call create on db.todo
-	//respond with 200 and todo
-	// res.status(400).json(e)
-
-
-	/*if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-		return res.status(400).send(); 
-	}
-
-	body.description = body.description.trim();
-	//set body.description as trimmed value
-
-	//add field to body
-	body.id = todoNextId++;
-
-	//push body
-	todos.push(body);
-
-	//console.log('description ' + body.description);
-	res.json(body);*/
 });
 
 
